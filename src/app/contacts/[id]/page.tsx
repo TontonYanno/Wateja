68a739920f6f8318f96dd50dd5ac11efa1b2c970
yann codeur <yanncodeur1@gmail.com>
2025-05-19 15:56:58 +0200

    architecture mvc assez particuliere

---
+++ src/app/contacts/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {}
+
+export default function page({}: Props) {
+    return (
+        <div>
+            <h1> Pages avec les id  </h1>
+        </div>
+    )
+}
